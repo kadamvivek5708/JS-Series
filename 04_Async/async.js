240af6b7d8f38e0fa3d8f40188feab6665bfc521
Vivek Kadam <144525243+kadamvivek5708@users.noreply.github.com>
2025-09-06 11:15:41 +0530

    Async in JS

---
+++ 04_Async/async.js
@@ -0,0 +1,21 @@
+
+//  Javascript --> Synchronas
+//             --> Single threded
+
+
+// ---> EXECUTION CONTEXT :
+//          -->Execute one line of code at a time
+//          --> (Each function wait for the last one to complete for execution)
+//          [call stack  ||  Memory Heap ]  <--- Global Execution Context
+
+
+// ____BLOCKING_CODE vs NONBLOCKING_CODE____
+// 
+// --> BLOCKING_CODE :
+//          -->Block the flow of program until current execution is completed
+//          -->READ FILE SYNC
+// 
+// --> NONBLOCKING_CODE :
+//          -->DO NOT Block the flow of program
+//          -->READ FILE ASYNC
+// 
