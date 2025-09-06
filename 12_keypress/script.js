7d156c496a5463a046f4ed6c0d763495ca15a35e
Vivek Kadam <144525243+kadamvivek5708@users.noreply.github.com>
2025-09-06 11:35:57 +0530

    Keypress Project

---
+++ 12_keypress/script.js
@@ -0,0 +1,21 @@
+console.log('Project 5');
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+  
+  <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+  
+  `;
+});
